29e3747a6937932924dbdfd5a3386686bb43a8e0
Hayk <kyutneryan803@gmail.com>
2021-09-22 11:53:31 +0400

    added head

--- src/Components/Pages/SignUpSignIn/SignIn/index.tsx
+++ src/Components/Pages/SignUpSignIn/SignIn/index.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 import { Layout } from 'antd';
 import Kingdom11 from "../../../../assets/Kingdom11.png";
 import LeftSide from '../LeftSide';
+import Head from '../Head';
 
 const { Header, Sider, Content } = Layout;
 
 const SignIn = () => {
     return (
         <Layout style={{ height: "100vh" }}>
-        <Sider width={444}>
+        <Sider style={{backgroundColor:'#000C17'}} width={444}>
         <LeftSide SiderImg={Kingdom11} />
       </Sider>
         <Layout>
-          <Header>Header</Header>
-          <Content>
+          <Header style={{backgroundColor:'white'}} >
+          <Head
+            HeaderText='Not a member?'
+            Link={<Link to='/signup'>Sign up</Link>}
+          />
+          </Header>
+          <Content style={{backgroundColor:'white'}}>
               SignIn
           </Content> 
         </Layout>

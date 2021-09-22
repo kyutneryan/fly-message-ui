7cb613bbb5e35bacc5dead20da27e1c5b286c5b0
Hayk <kyutneryan803@gmail.com>
2021-09-22 12:25:44 +0400

    changed antd default variables

--- craco.config.js
+++ craco.config.js
@@ -7,7 +7,13 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
-            modifyVars: { '@primary-color': '#1DA57A' },
+            modifyVars: {
+              "@primary-color": "#38D996",
+              "@layout-header-padding": "0 48px",
+              "@layout-header-background": "#000C17",
+              "@layout-header-height": "112px",
+              "@form-item-margin-bottom": "12px",
+            },
             javascriptEnabled: true,
           },
         },

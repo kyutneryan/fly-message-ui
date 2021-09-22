a66ef617cbaeb27b19300414252658da0f2c639d
Hayk <kyutneryan803@gmail.com>
2021-09-22 16:46:51 +0400

    added cover picture

---
+++ src/Components/Pages/Profile/CoverPicture/index.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import { Upload, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import ImgCrop from "antd-img-crop";
+
+function getBase64(file: Blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
+
+const CoverPicture = () => {
+  const [previewImage, setPreviewImage] = useState("");
+  const [fileList, setFileList] = useState([]);
+
+  const handlePreview = async ({ fileList }: any) => {
+    if (!fileList[0].preview) {
+      fileList[0].preview = await getBase64(fileList[0].originFileObj);
+      console.log(fileList[0]);
+    }
+    setPreviewImage(fileList[0].preview);
+    setFileList(fileList);
+  };
+  return (
+    <div
+      style={{
+        height: "300px",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
+        padding: 24,
+        backgroundColor: "#fff",
+        backgroundImage: `url(${previewImage})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <ImgCrop rotate aspect={2.5}>
+        <Upload
+          maxCount={1}
+          action={(file: Blob) =>
+            new Promise((resolve, reject) => {
+              const reader: any = new FileReader();
+              reader.readAsDataURL(file);
+              reader.onload = () => resolve(reader.result);
+              reader.onerror = (error: string) => reject(error);
+            })
+          }
+          showUploadList={false}
+          fileList={fileList}
+          onChange={handlePreview}
+        >
+          <Button
+            type='primary'
+            size='small'
+            icon={<UploadOutlined />}
+          ></Button>
+        </Upload>
+      </ImgCrop>
+    </div>
+  );
+};
+
+export default CoverPicture;

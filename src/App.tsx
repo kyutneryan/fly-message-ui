bc3c5e8d89e27fcc7d9e265af8e9e3f0daf559d9
Hayk <kyutneryan803@gmail.com>
2021-09-22 15:34:47 +0400

    changed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React, { FC } from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-
+  Redirect,
 } from "react-router-dom";
-import './App.less';
-import SignIn from './Components/Pages/SignUpSignIn/SignIn';
-import SignUp from './Components/Pages/SignUpSignIn/SignUp'
+import "./App.less";
+import Home from "./Components/Pages/Profile/Home";
+import Profile from "./Components/Pages/Profile/ProfilePage";
+import SignIn from "./Components/Pages/SignUpSignIn/SignIn";
+import SignUp from "./Components/Pages/SignUpSignIn/SignUp";
 
-const App: FC = () => (
+const App = () => (
   <Router>
-   <Switch>
-  <Route exact path='/'>
+    <Switch>
+      <Route exact path='/'>
         <SignIn />
-  </Route>
-  <Route path='/signup'>
-    <SignUp />
-  </Route>
-  </Switch>
+      </Route>
+      <Route path='/signup'>
+        <SignUp />
+      </Route>
+      <Redirect from='/signin' to='/' />
+      <Route path='/profile'>
+        <Profile />
+      </Route>
+      <Route path='/home'>
+        <Home />
+      </Route>
+    </Switch>
   </Router>
-  
 );
 
 export default App;
